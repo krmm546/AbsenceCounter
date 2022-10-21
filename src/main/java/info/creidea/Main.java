ca98fcd066c35f1c946789b09f94439661213bda
kuromame <moeruensyo0546@gmail.com>
2022-10-21 12:11:39 +0900

    Migrationの実行

--- src/main/java/info/creidea/Main.java
+++ src/main/java/info/creidea/Main.java
@@ -2,11 +2,16 @@ package info.creidea;
 
 import info.creidea.controller.AbsenceViewController;
 import info.creidea.controller.LoginController;
+import info.creidea.database.DatabaseFactory;
+import info.creidea.database.migration.AllMigration;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        final var connection = DatabaseFactory.create();
+        new AllMigration().prepare(connection);
 
         final var templateEngine = new VelocityTemplateEngine();
         final var login = new LoginController();
