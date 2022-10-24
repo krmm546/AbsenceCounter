43b23fdec2b5eeb9da5506c40e04debaf793a975
kuromame <moeruensyo0546@gmail.com>
2022-10-24 09:32:09 +0900

    HTMLを追加

--- src/main/java/info/creidea/Main.java
+++ src/main/java/info/creidea/Main.java
@@ -10,14 +10,20 @@ import info.creidea.database.migration.AllMigration;
 import info.creidea.database.query.AbsenceFetcher;
 import info.creidea.database.query.AbsenceRegister;
 import info.creidea.database.query.Authenticator;
+import info.creidea.database.query.Logger;
+import info.creidea.database.seed.SampleSeeder;
+import info.creidea.middleware.LoggerMiddleware;
+import info.creidea.middleware.LoginMiddleware;
 import spark.template.velocity.VelocityTemplateEngine;
-
 import java.sql.SQLException;
 
+import static spark.Spark.*;
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, InterruptedException {
+        // Databases
         final var connection = DatabaseFactory.create();
         new AllMigration().prepare(connection);
+        new SampleSeeder().seed(connection);
 
         // Dependencies
         final var authenticator = new Authenticator(connection);
@@ -25,11 +31,26 @@ public class Main {
         final var absenceFetcher = new AbsenceFetcher(connection);
         final var subjectAbsenceFetcher = new SubjectAbsenceFetcher(connection, subjectsFetcher);
         final var absenceRegister = new AbsenceRegister(connection);
+        final var logger = new Logger(connection);
 
         final var templateEngine = new VelocityTemplateEngine();
+
+        // Controllers
         final var login = new LoginController(authenticator);
         final var absence = new AbsenceViewController(subjectsFetcher, absenceFetcher);
         final var register = new RegisterController(subjectAbsenceFetcher, absenceRegister);
+
+        // Middlewares
+        final var logMiddleware = new LoggerMiddleware(logger);
+        final var loginMiddleware = new LoginMiddleware();
+
+
+        staticFiles.location("/public");
+
+        before("*", logMiddleware::before);
+        before("/", loginMiddleware::before);
+        before("/register", loginMiddleware::before);
+        before("/register/*", loginMiddleware::before);
 
         login.boot(templateEngine);
         absence.boot(templateEngine);
--- src/main/java/info/creidea/database/DatabaseFactory.java
+++ src/main/java/info/creidea/database/DatabaseFactory.java
@@ -5,11 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseFactory {
-    private static final String  URL = "jdbc:mysql://localhost:8889/absence";
+    private static final String  URL = "jdbc:mysql://db:3306/absence";
     private static final String USER = "jdbc_username";
     private static final String PASSWORD = "jdbc_password";
-    public static Connection create() throws SQLException {
+    public static Connection create() throws SQLException, InterruptedException {
+        System.out.println("Docker Database Pausing...");
+        Thread.sleep(10000);
+        System.out.println("Connecting....");
+        final var connection = DriverManager.getConnection(URL, USER, PASSWORD);
         System.out.println("Connected....");
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return connection;
     }
 }
